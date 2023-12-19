2402039097f9670ba329d88298babc3b2c658ad5
Moyses Levi Zalewski Pietsch dos Santos <moyzlevi@gmail.com>
2023-12-19 15:37:56 -0300

    Create validator

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,16 +6,15 @@ import (
 	"net/http"
 	"strconv"
 	"snippetbox.moypietsch.com/internal/models"
+	"snippetbox.moypietsch.com/internal/validator"
 	"github.com/julienschmidt/httprouter"
-	"strings"
-	"unicode/utf8"
 )
 
 type snippetCreateForm struct {
 	Title string
 	Content string
 	Expires int
-	FieldErrors map[string]string
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -79,26 +78,24 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		Title: r.PostForm.Get("title"),
 		Content: r.PostForm.Get("content"),
 		Expires: expires,
-		FieldErrors: map[string]string{},
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title),"title", 
+	"This field cannot be blank")
+	
+	form.CheckField(validator.MaxChars(form.Title, 100),"title", 
+	"This field cannot be more than 100 characters long")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
+	form.CheckField(validator.NotBlank(form.Content),"content", 
+	"This field cannot be blank")
 
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must be equal 1, 7 or 365"
-	}
+	form.CheckField(validator.PermittedInt(form.Expires, 1 ,7, 365),"expires", 
+	"This field must be equal 1, 7 or 365")	
 
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
+		fmt.Printf("", form.FieldErrors)
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
 		return
 	}
